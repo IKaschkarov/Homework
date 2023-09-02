adb47c2be3dd132f336111c1fedf0e0c3b49319b
IKaschkarov <goldonosik@gmail.com>
2023-09-02 22:03:44 +0200

    lesson2

--- src/main/java/de/telran/lesson2/TestScanner.java
+++ src/main/java/de/telran/lesson2/TestScanner.java
@@ -6,18 +6,18 @@ public class TestScanner {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите число");
+        System.out.print("Введите целое число:");
        int myInt = scanner.nextInt();
        int mySum = myInt + 10;
         System.out.println(mySum);
 
-        System.out.println("Введите число");
+        System.out.print("Введите дробное число:");
         double myDouble = scanner.nextDouble();
         System.out.println(myDouble-10);
 
         Scanner scannerStr = new Scanner(System.in);
 
-        System.out.println("Введите текст");
+        System.out.print("Введите текст:");
         String myStr = scannerStr.nextLine();
         System.out.println(myStr + "!!!");
 
