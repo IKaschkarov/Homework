7a0ed27d4e1a38dc4b1eb12d8df4cf7e81bdb6ad
IKaschkarov <goldonosik@gmail.com>
2023-09-05 21:28:03 +0200

    lesson4

---
+++ src/main/java/de/telran/lesson4/CalculatorDemo.java
@@ -0,0 +1,28 @@
+package de.telran.lesson4;
+
+     import java.util.Scanner;
+public class CalculatorDemo {
+
+        public static void main(String[] args) {
+
+            Calculator calc = new Calculator(); // создаем новый экземпляр класса "Calculator" - calc
+            Scanner enter = new Scanner(System.in); // создаем новый экземпляр класса "Scanner" - enter
+
+
+            System.out.print("Введите первое число (возможно дробное): ");
+            double x = enter.nextDouble(); //  ввод 1-го числа с клавиатуры
+            System.out.print("Введите второе число (возможно дробное): ");
+            double y = enter.nextDouble(); //  ввод 2-го числа с клавиатуры
+
+
+
+            System.out.println(x + " / " + y + " = " + calc.div(x, y));
+            System.out.println(x + " * " + y + " = " + calc.mul(x, y));
+            System.out.println(x + " - " + y + " = " + calc.sub(x, y));
+            System.out.println(x + " + " + y + " = " + calc.sum(x, y));
+
+
+
+        }
+
+}
---
+++ src/main/java/de/telran/lesson4/Text.java
@@ -0,0 +1,26 @@
+package de.telran.lesson4;
+
+public class Text {
+        public static void main(String[] args) {
+
+
+            String text = new String("I study Basic Java!"); // Создаем строку через new
+            Method(text); // передаем в метод строку I study Basic Java!
+
+            System.out.println("Количество символов: " + text.length());
+            System.out.println("Предпоследняя буква: " + text.charAt(text.length() - 2)); // предпоследний символ строки
+            System.out.println("Содержит слово \"Java\"?: " + text.contains("Java")); // Проверяем, содержит ли ваша строка подстроку “Java”
+            System.out.println(text.substring(14,18)); // Вырезаем подстроку Java из строки
+            System.out.println(text.substring(0,14) + text.substring(18)); //
+            System.out.println(text.replace("a","o")); // Меняем все символы “а” на “о”
+            System.out.println(text.toUpperCase()); // Преобразуем строку к верхнему регистру
+            System.out.println(text.toLowerCase()); // Преобразуем строку к нижнему регистру
+        }
+
+        public static String Method(String txt) { //метод, который обрабатывает текст и возвращает переменной text
+            return txt + "!!";
+        }
+    }
+
+
+
