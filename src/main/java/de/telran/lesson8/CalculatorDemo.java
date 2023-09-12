646fd6f27b4289c0c7400e93d748759d2943ded1
IKaschkarov <goldonosik@gmail.com>
2023-09-12 09:14:42 +0200

    Homework 05/09

--- src/main/java/de/telran/lesson8/CalculatorDemo.java
+++ src/main/java/de/telran/lesson8/CalculatorDemo.java
@@ -7,14 +7,14 @@ public class CalculatorDemo {
 
         public static void main(String[] args) {
 
-            Calculator calc = new Calculator(); // создаем новый экземпляр класса "Calculator" - calc
-            Scanner enter = new Scanner(System.in); // создаем новый экземпляр класса "Scanner" - enter
+            Calculator calc = new Calculator();
+            Scanner enter = new Scanner(System.in);
 
 
             System.out.print("Введите первое число (возможно дробное): ");
-            double x = enter.nextDouble(); //  ввод 1-го числа с клавиатуры
+            double x = enter.nextDouble();
             System.out.print("Введите второе число (возможно дробное): ");
-            double y = enter.nextDouble(); //  ввод 2-го числа с клавиатуры
+            double y = enter.nextDouble();
 
 
 
