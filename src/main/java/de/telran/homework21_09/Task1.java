0929c82d5a270f314512c57c38d73288d16473fd
IKaschkarov <goldonosik@gmail.com>
2023-09-29 07:57:21 +0200

    Homework 21/09

--- src/main/java/de/telran/homework21_09/Task1.java
+++ src/main/java/de/telran/homework21_09/Task1.java
@@ -12,13 +12,20 @@ public class Task1 {
         String[] myArray = {"a", "b", "c", "d", "e"};
 
         System.out.println("Прямой порядок элементов массива:" + Arrays.toString(myArray));
+        //int length = myArray.length;
+
+        reverseArray(myArray);
+        System.out.print("Обратный порядок элементов массива: " + Arrays.toString(myArray));
+
+    }
+    private static void reverseArray(String[] myArray) {
         int length = myArray.length;
+        for (int i = 0; i < length / 2; i++) {
 
-        System.out.print("Обратный порядок элементов массива: ");
-
-        for (int i = length - 1; i >= 0; i--) {
-
-            System.out.print( myArray[i] + ", ");
+            String temp = myArray[i];
+            myArray[i] = myArray[length - 1 - i];
+            myArray[length - 1 - i] = temp;
         }
     }
 }
+
